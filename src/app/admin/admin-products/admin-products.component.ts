49f3123a01d4ddd413853ce813e65b123ef7994e
Herri Fransisca <herrifransisca@outlook.com>
2017-09-07 11:40:02 +0700

    Searching for Products.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,15 +1,29 @@
 import { ProductService } from './../../product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent {
-  products$;
+export class AdminProductsComponent implements OnDestroy {
+  products: { title: string }[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(productService: ProductService) {
-    this.products$ = productService.getAll();
+    this.subscription = productService.getAll()
+      .subscribe(products => this.filteredProducts = this.products = products);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+      this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
   }
 }

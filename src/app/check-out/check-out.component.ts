f4f4515d62b54c63e19a3a473a460a71242e9480
Herri Fransisca <herrifransisca@outlook.com>
2017-09-13 22:17:50 +0700

    Redirecting the User to the Order Success Page.

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -6,6 +6,7 @@ import { ShoppingCart } from '../models/shopping-cart';
 import { ShoppingCartService } from '../shopping-cart.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Order } from '../models/order';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'check-out',
@@ -20,6 +21,7 @@ export class CheckOutComponent implements OnInit, OnDestroy {
   userSubscription: Subscription;
 
   constructor(
+    private router: Router,
     private authService: AuthService,
     private orderService: OrderService,
     private shoppingCartService: ShoppingCartService) { }
@@ -36,8 +38,9 @@ export class CheckOutComponent implements OnInit, OnDestroy {
     this.userSubscription.unsubscribe();
   }
 
-  placeOrder() {
+  async placeOrder() {
     let order = new Order(this.userId, this.shipping, this.cart);
-    this.orderService.storeOrder(order);
+    let result = await this.orderService.storeOrder(order);
+    this.router.navigate(['/order-success', result.key]);
   }
 }

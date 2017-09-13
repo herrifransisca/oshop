02d48b67ebfa22291b413dc704901c1b93ededd3
Herri Fransisca <herrifransisca@outlook.com>
2017-09-13 22:29:58 +0700

    Clearing the Shopping Cart.

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,12 +1,17 @@
+import { ShoppingCartService } from './shopping-cart.service';
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 
 @Injectable()
 export class OrderService {
 
-  constructor(private db: AngularFireDatabase) { }
+  constructor(
+    private shoppingCartService: ShoppingCartService,
+    private db: AngularFireDatabase) { }
 
-  storeOrder(order) {
-    return this.db.list('/orders').push(order);
+  async placeOrder(order) {
+    let result = await this.db.list('/orders').push(order);
+    this.shoppingCartService.clearCart();
+    return result;
   }
 }

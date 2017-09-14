9686d68d95bbc5bf875d873f15f48ab42803046a
Herri Fransisca <herrifransisca@outlook.com>
2017-09-14 15:35:52 +0700

    Creating the Shopping Module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ShoppingModule } from './shopping/shopping.module';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -15,36 +16,21 @@ import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './core/components/bs-navbar/bs-navbar.component';
 import { HomeComponent } from './core/components/home/home.component';
 import { LoginComponent } from './core/components/login/login.component';
-import { AuthGuard } from './shared/services/auth.guard';
 import { SharedModule } from './shared/shared.module';
-import { CheckOutComponent } from './shopping/components/check-out/check-out.component';
-import { MyOrdersComponent } from './shopping/components/my-orders/my-orders.component';
-import { OrderSuccessComponent } from './shopping/components/order-success/order-success.component';
-import { ProductFilterComponent } from './shopping/components/products/product-filter/product-filter.component';
 import { ProductsComponent } from './shopping/components/products/products.component';
-import { ShippingFormComponent } from './shopping/components/shipping-form/shipping-form.component';
-import { ShoppingCartSummaryComponent } from './shopping/components/shopping-cart-summary/shopping-cart-summary.component';
-import { ShoppingCartComponent } from './shopping/components/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BsNavbarComponent,
     HomeComponent,
-    ProductsComponent,
-    ShoppingCartComponent,
-    CheckOutComponent,
-    OrderSuccessComponent,
-    MyOrdersComponent,
     LoginComponent,
-    ProductFilterComponent,
-    ShoppingCartSummaryComponent,
-    ShippingFormComponent
   ],
   imports: [
     BrowserModule,
     SharedModule,
     AdminModule,
+    ShoppingModule,
     FormsModule,
     DataTableModule,
     AngularFireModule.initializeApp(environment.firebase),
@@ -54,13 +40,7 @@ import { ShoppingCartComponent } from './shopping/components/shopping-cart/shopp
     NgbModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: ProductsComponent },
-      { path: 'products', component: ProductsComponent },
-      { path: 'shopping-cart', component: ShoppingCartComponent },
       { path: 'login', component: LoginComponent },
-
-      { path: 'check-out', component: CheckOutComponent, canActivate: [AuthGuard] },
-      { path: 'order-success/:id', component: OrderSuccessComponent, canActivate: [AuthGuard] },
-      { path: 'my/orders', component: MyOrdersComponent, canActivate: [AuthGuard] }
     ])
   ],
   providers: [],
